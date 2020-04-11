ac9001a3e773e6f9d310da8ef35968fb91a0fcf7
Yurafobus1 <agafonovypfobus@gmail.com>
2020-04-11 20:47:12 +0300

    code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
-#include <SFML/Main.hpp>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    
+    sf::Window window;
+    window.create(sf::VideoMode(1280, 720), "Chess");
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(false);
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        for (size_t j = 0; j < 8; j++)
+        {
+            sf::RectangleShape block;
+            block.setSize(sf::Vector2f(75, 75));
+            block.setPosition(i*75, j*75);
+            block.setFillColor(sf::Color(0 - (i + j % 2)));
+            //window.draw(block);
+        }
+            
+    }
+
+    //char a = getchar();
+    return 0;
 }
