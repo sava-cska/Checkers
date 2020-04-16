5469a8348df3742d15ac175a5956c8c7c4c3da11
Grigoryev <sava-cska@mail.ru>
2020-04-16 12:32:46 -0400

    Добавил пример makefile для сборки

---
+++ model/main.cpp
@@ -0,0 +1,12 @@
+#include "game.hpp"
+
+int main()
+{
+	std::string s, t;
+	std::cin >> s >> t;
+	Game game;
+	game.load_from_file(s);
+	game.move(FIRST, board_cell(5, 0), board_cell(4, 1));
+	game.save_to_file(t);
+	return 0;
+}
