e75eaa58c486d60e8b20bbcb3efab9a3a0c592f8
sergeypospelov <sergeypospelov59@gmail.com>
2020-04-22 18:51:15 +0300

    + merge network

--- main.cpp
+++ main.cpp
@@ -2,59 +2,78 @@
 #include "Player.hpp"
 #include "draw_smf.hpp"
 #include "game.hpp"
+#include "Network.hpp"
 
 #include <iostream>
 #include <string>
+#include <cassert>
 
-int main() {
-  Gra core;
-  //  Network network;
-
-  std::string mode;
-  std::cout << "Game mode" << std::endl;
+static void choose_game_mode(controller::IPlayer *&player,
+                             controller::IPlayer *&enemy, Network &network, Game_state game_state, std::string &mode) {
+  std::cout << "Game mode (single | server | client)" << std::endl;
   std::cin >> mode;
+  if (mode == "single") {
+    player = new controller::Player(number_of_player::FIRST, game_state);
+    enemy = new controller::Player(number_of_player::SECOND, game_state);
+  } else if (mode == "server") {
+    player = new controller::Player(number_of_player::FIRST, game_state);
+    enemy = new controller::NetworkPlayer(number_of_player::SECOND, network);
+    network.setup_server();
+  } else if (mode == "client") {
+    std::string ip;
+    std::cout << "Ip address" << std::endl;
+    std::cin >> ip;
+    player = new controller::Player(number_of_player::SECOND, game_state);
+    enemy = new controller::NetworkPlayer(number_of_player::FIRST, network);
+    network.connect_to_player(ip);
+  }
+} // TODO: make special class for it!
+
+int main() { // TODO: make own main for every game mode
+  Gra core;
+  Network network;
 
   Game_state game_state;
-  Controller::IPlayer *player =
-      new Controller::Player(number_of_player::FIRST, game_state);
-  Controller::IPlayer *enemy = nullptr;
+  controller::IPlayer *player = nullptr;
+  controller::IPlayer *enemy = nullptr;
 
-  if (mode == "multi") {
-    //    enemy = new Controller::NetworkPlayer(number_of_turn::SECOND,
-    //    network);
-  } else if (mode == "single") {
-    enemy = new Controller::Player(number_of_player::SECOND, game_state);
-  }
+  std::string mode;
+  choose_game_mode(player, enemy, network, game_state, mode);
 
-  sf::RenderWindow window;
+  assert(player != nullptr);
+  assert(enemy != nullptr);
 
   while (core.window.isOpen()) {
-    core.update(game_state);
+    if (mode == "single" && game_state.who_moves() == enemy->turn) {
+      core.update(game_state, enemy);
+    } else {
+      core.update(game_state, player);
+    }
     core.compiling_event(game_state);
+    network.update();
 
     while (!core.get_events().empty()) {
       controller::Event *event = core.get_events().front();
       controller::MoveEvent *move =
           dynamic_cast<controller::MoveEvent *>(event);
-      // example of processing.
-      // TODO: This code should be encapsulated in MoveEvent.
-      if (move != nullptr) {
-        if (mode == "single") {
-          if (game_state.who_moves() == player->turn) {
-            player->add_move(move->from, move->to);
-          } else {
-            enemy->add_move(move->from, move->to);
-          }
-        } else {
-          if (game_state.who_moves() == player->turn) {
-            player->add_move(move->from, move->to);
-          }
-        }
-        core.get_events().pop();
-      }
+      controller::process(move, player, enemy, game_state,
+                          mode); // overloaded in Event.hpp
 
       // currently, there is only MoveEvent.
       // TODO: need to process another events!
+      core.get_events().pop();
+    }
+
+    while (!network.get_events().empty()) {
+      controller::Event *event = network.get_events().front();
+      controller::MoveEvent *move =
+          dynamic_cast<controller::MoveEvent *>(event);
+      controller::process(move, enemy, player, game_state,
+                          mode); // overloaded in Event.hpp
+
+      // currently, there is only MoveEvent.
+      // TODO: need to process another events!
+      network.get_events().pop();
     }
 
     while (player->check_move()) {
@@ -68,6 +87,7 @@ int main() {
       auto move = enemy->get_move();
       player->send_move(move.first, move.second);
       game_state.move(enemy->turn, move.first, move.second);
+      std::cerr << "MOVE!\n";
       enemy->pop_move();
     }
 
