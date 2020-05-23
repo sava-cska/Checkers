dcbdd4268f7e0750deef485af102109c80b811d9
sergeypospelov <sergeypospelov59@gmail.com>
2020-05-23 16:18:12 +0300

    very stupid text interface

---
+++ main_text.cpp
@@ -0,0 +1,21 @@
+#include "CompPlayer.hpp"
+#include "Event.hpp"
+#include "Game.hpp"
+#include "Graphics.hpp"
+#include "Network.hpp"
+#include "Player.hpp"
+
+#include "TextView.hpp"
+
+#include "tclap/CmdLine.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char **argv) { // TODO: make own main for every game mode
+  TextView tv(argc, argv);
+
+  return 0;
+}
